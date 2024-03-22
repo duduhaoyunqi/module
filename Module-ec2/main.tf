6f4070b5cd24d9ae09e6420d45e77e9b56bc6887
duduhaoyunqi <88752377+duduhaoyunqi@users.noreply.github.com>
2024-03-22 13:36:48 +0800

    Update main.tf

--- Module-ec2/main.tf
+++ Module-ec2/main.tf
@@ -1,5 +1,7 @@
 resource "aws_instance" "this" {
+  # 接收到instances这个variable后遍历
   for_each = var.instances
+
   # 左边的attribute是terraform aws provider定义好的，不能自定义名称。
   ami           = each.value.ami
   instance_type = each.value.type
