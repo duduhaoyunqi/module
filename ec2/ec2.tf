2560f2f50f9ef06689f7cd9f1cfea78d3fb2a9f1
duduhaoyunqi <88752377+duduhaoyunqi@users.noreply.github.com>
2024-03-22 13:32:22 +0800

    Update ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -8,3 +8,21 @@ module "ec2_instance" {
     source = "..\\Module-ec2"
     instances = local.ec2_instances
 }
+
+# 查看返回的object结果
+# $ terraform console
+# > {for instance in local.ec2_config.ec2_instances: instance.name => instance}
+# {
+#   "wpfblogirbrd01" = {
+#     "ami" = "ami-09b1e8fc6368b8a3a"
+#     "name" = "wpfblogirbrd01"
+#     "subnet_id" = "subnet-0bd4d44232932bbc3"
+#     "type" = "t2.micro"
+#   }
+#   "wpfblogirbrd02" = {
+#     "ami" = "ami-09b1e8fc6368b8a3a"
+#     "name" = "wpfblogirbrd02"
+#     "subnet_id" = "subnet-0bd4d44232932bbc3"
+#     "type" = "t2.micro"
+#   }
+# }
