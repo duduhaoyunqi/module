6e20cc9b9285ce8498de4ec36047d14bdefe9697
duduhaoyunqi <88752377+duduhaoyunqi@users.noreply.github.com>
2024-03-22 13:30:14 +0800

    Update main.tf

--- Module-ec2/main.tf
+++ Module-ec2/main.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "this" {
   for_each = var.instances
+  # 左边的attribute是terraform aws provider定义好的，不能自定义名称。
   ami           = each.value.ami
   instance_type = each.value.type
   subnet_id = each.value.subnet_id
