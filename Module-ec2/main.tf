da5dc1eac2d68d395ca90595fe9e026f8b525d68
duduhaoyunqi <88752377+duduhaoyunqi@users.noreply.github.com>
2024-03-27 15:23:30 +0800

    Update main.tf
    updated

--- Module-ec2/main.tf
+++ Module-ec2/main.tf
@@ -1,3 +1,11 @@
+data "aws_subnet" "ec2_instance" {
+  for_each = var.instances
+  filter {
+    name   = "tag:Name"
+    values = [each.value.subnet]
+  }
+}
+
 resource "aws_instance" "this" {
   # 接收到instances这个variable后遍历
   for_each = var.instances
@@ -5,5 +13,6 @@ resource "aws_instance" "this" {
   # 左边的attribute是terraform aws provider定义好的，不能自定义名称。
   ami           = each.value.ami 
   instance_type = each.value.type  # extract "type" attribute from each object
-  subnet_id = each.value.subnet_id  
+  subnet_id = data.aws_subnet.ec2_instance[each.key].id
+  key_name  = try(each.value.key_name, null)
 }
